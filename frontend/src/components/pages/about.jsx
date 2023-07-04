4abfe1b8703f1c89e3bdc187346723c56c76df47
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-04 15:13:02 +0100

    Company history new UI

--- frontend/src/components/pages/about.jsx
+++ frontend/src/components/pages/about.jsx
@@ -2,9 +2,9 @@ import PageTitle from "../cards/pageTitle";
 import CoreValueGroup from "../groups/coreValueGroup";
 import StatGroup from "../groups/statGroup";
 import WhyChooseUs from "../cards/whyChooseUs";
-import TextListTemplate from "../cards/textTemplate";
 import { useContext } from "react";
 import { CompanyInformationContext } from "../../App";
+import parse from 'html-react-parser';
 
 const About = () => {
     const companyInfo = useContext(CompanyInformationContext)
@@ -16,13 +16,13 @@ const About = () => {
 
             {companyInfo.company_history
                 ?
-                <section className="py-8 history">
-                    <TextListTemplate
-                        title='Our History'
-
-                        description={companyInfo.company_history}
-                        pic={companyInfo.get_history_image}
-                    />
+                <section className="container py-8">
+                    <section className="row">
+                        <section className="col-lg-8 mx-auto">
+                            <h2 className="text-center">Our History</h2>
+                            {parse(`${companyInfo.company_history}`)}
+                        </section>
+                    </section>
                 </section>
                 :
                 null
