74e55c939c1649d5d5f032f40ff1afee45c46fb7
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-08 22:13:44 +0100

    Minor changes

--- frontend/src/components/detailed/serviceDetail.jsx
+++ frontend/src/components/detailed/serviceDetail.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import parse from 'html-react-parser';
- 
+
 const ServiceDetail = () => {
     const { slug } = useParams();
     const [detail, setDetails] = useState([]);
