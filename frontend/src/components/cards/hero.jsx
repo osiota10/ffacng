8e4097c0330b21c635736c242b82da7e14f665fe
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2023-07-12 18:50:50 +0100

    Code clean up

--- frontend/src/components/cards/hero.jsx
+++ frontend/src/components/cards/hero.jsx
@@ -22,14 +22,6 @@ const settings = {
     },
 };
 
-const myStyle = {
-    // backgroundImage: `url(${pic.url})`,
-    backgroundImage: `linear-gradient(to right, rgb(3, 98, 63), rgba(3, 98, 63, 0.3)), url(${pic.url})`,
-    height: '100%',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    // boxShadow: 'inset 0 0 0 100vh rgba(0, 0, 0, 0.6)',
-};
 
 const getStyle = (imgUrl) => {
     return {
@@ -63,18 +55,16 @@ const Hero = () => {
                         </section>
 
                         <div className="container position-absolute top-50 start-50 translate-middle py-5">
-                            <section className="custom-slider">
-                                <div className="row">
-                                    <div className="col-lg-6">
-                                        <h2 className="mb-1 text-white">{item.title}</h2>
-                                        <p className="text-white">{item.snippet}</p>
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <h2 className="mb-1 text-white">{item.title}</h2>
+                                    <p className="text-white">{item.snippet}</p>
 
-                                        <div>
-                                            <Link to="/contact" className='btn btn-light mt-3'>Contact Us</Link>
-                                        </div>
+                                    <div>
+                                        <Link to="/contact" className='btn btn-light mt-3'>Contact Us</Link>
                                     </div>
                                 </div>
-                            </section>
+                            </div>
                         </div>
                     </section>
                 )}
